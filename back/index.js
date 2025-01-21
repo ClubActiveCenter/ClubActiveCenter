cbe97d3625d0b63e4e408025976f0c91a5dbf070
unknown <acarneri20@gmail.com>
2025-01-21 13:04:55 -0300

    nuevaFuncionSumar

--- back/index.js
+++ back/index.js
@@ -1,5 +1,15 @@
-const sumar = (num, num2) => {
-    return num + num2
-}
 
-console.log(sumar(1, 2))
+
+// sumar solo los numeros pares
+const sumarPares = (arrayNum) =>{
+
+    const pares = arrayNum.filter((num) => num % 2 == 0);
+    
+    let total = 0
+    
+    for(let i = 0 ; i < pares.length ; i++){
+     total += pares[i]
+    
+    }
+}
+console.log(sumarPares([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
