cb998c87bd78bb5330f9364f51019219e9d1be8e
agustintoledo2908 <agustintoledo2908@gmail.com>
2025-01-28 01:52:32 -0300

    se soluciono  el error de implementación

--- back/active-center/src/Auth/auth.module.ts
+++ back/active-center/src/Auth/auth.module.ts
@@ -5,18 +5,11 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/Entities/User.entity';
 import { UserModule } from 'src/User/user.module';
-import { SendGridService } from 'src/SendGrid/sendGrid.service';
 import { SendGridModule } from 'src/SendGrid/sendGrid.module';
-
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, SendGridService],
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    UserModule,
-    SendGridModule
-  ]
-
+  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User]), UserModule, SendGridModule],
 })
 export class AuthModule {}
